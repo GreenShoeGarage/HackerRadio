115bafb7413f10b805eddec08bfbc2129c6a6cc9
mike parks <mike@greenshoegarage.com>
2020-12-03 00:27:29 -0500

    added 404 error handler

--- Software/HackerRadio_Firmware/HackerRadio_Firmware.ino
+++ Software/HackerRadio_Firmware/HackerRadio_Firmware.ino
@@ -1,24 +1,23 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <FS.h>   // Include the SPIFFS library
-#include <Adafruit_Si4713.h>  //must edit Adafruit_Si4713.cpp if using NodeMCU. Must edit the begin() function to change the I2C SDA and SCL pins    _wire->pins(4,5); 
-#include "secrets.h"   
-
+#include <FS.h>              // Include the SPIFFS library
+#include <Adafruit_Si4713.h> //must edit Adafruit_Si4713.cpp if using NodeMCU. Must edit the begin() function to change the I2C SDA and SCL pins    _wire->pins(4,5);
+#include "secrets.h"
 
 #define _BV(n) (1 << n)
 #define RESETPIN 12
 #define BAUDRATE 115200
-#define MAX_TX_POWER 115  //88-115 MAX
+#define MAX_TX_POWER 115 //88-115 MAX
 #define DEFAULT_FREQ 10230
 #define IP_PORT 80
 
 #define DEBUG
 
 #ifdef DEBUG
-#define DEBUG_PRINTLN(x)         Serial.println (x)
-#define DEBUG_PRINT(x)           Serial.print(x)
-#define DEBUG_PRINTHEX(x, HEX)   Serial.print(x, HEX)
+#define DEBUG_PRINTLN(x) Serial.println(x)
+#define DEBUG_PRINT(x) Serial.print(x)
+#define DEBUG_PRINTHEX(x, HEX) Serial.print(x, HEX)
 #else
 #define DEBUG_PRINTLN(x)
 #define DEBUG_PRINT(x)
@@ -29,30 +28,25 @@ Adafruit_Si4713 radio = Adafruit_Si4713(RESETPIN);
 
 AsyncWebServer server(IP_PORT);
 
-unsigned int FMSTATION = DEFAULT_FREQ;      // 10230 == 102.30 MHz
+unsigned int FMSTATION = DEFAULT_FREQ; // 10230 == 102.30 MHz
 
 String hr_version = "v0.0.2";
 
-
-
-
-
-
 ////////////////////////////////////////////////////////////////////////////////////
-void printRadioInfo() {
+void printRadioInfo()
+{
   radio.readTuneStatus();
   DEBUG_PRINT("Current TX Frequency: ");
   printFrequency(radio.currFreq);
-  DEBUG_PRINT("\tCurrent ASQ: 0x");   DEBUG_PRINTHEX(radio.currASQ, HEX);
-  DEBUG_PRINT("\tCurrent InLevel:");  DEBUG_PRINTLN(radio.currInLevel);
+  DEBUG_PRINT("\tCurrent ASQ: 0x");
+  DEBUG_PRINTHEX(radio.currASQ, HEX);
+  DEBUG_PRINT("\tCurrent InLevel:");
+  DEBUG_PRINTLN(radio.currInLevel);
 }
 
-
-
-
-
 ////////////////////////////////////////////////////////////////////////////
-void setup() {
+void setup()
+{
   initSerial();
   initWifi();
   initSPIFFS();
@@ -60,58 +54,58 @@ void setup() {
   DEBUG_PRINTLN(F("\n\nWaiting for clients to connect..."));
 }
 
-
-
-
 ////////////////////////////////////////////////////////////////////////////
-void loop() {
+void loop()
+{
 }
 
-
-
-
 //////////////////////////////////////////////////////////////////////////
-void initSerial() {
+void initSerial()
+{
   Serial.begin(BAUDRATE);
   delay(10);
   DEBUG_PRINT(F("\n\nWelcome to Hacker Radio "));
   DEBUG_PRINTLN(hr_version);
 }
 
-
-
-
 ///////////////////////////////////////////////////////////////////////////
-void initWifi() {
+void initWifi()
+{
   DEBUG_PRINTLN(F("Initialzing wifi..."));
   WiFi.mode(WIFI_AP);
-  WiFi.softAP(ssid, password);             // Start the access point
+  WiFi.softAP(ssid, password); // Start the access point
   DEBUG_PRINT("Access Point \"");
   DEBUG_PRINT(ssid);
   DEBUG_PRINTLN("\" started");
 
   DEBUG_PRINT("IP address:\t");
-  DEBUG_PRINTLN(WiFi.softAPIP());         // Send the IP address of the ESP8266 to the computer
+  DEBUG_PRINTLN(WiFi.softAPIP()); // Send the IP address of the ESP8266 to the computer
 
-
-  server.on("/", HTTP_ANY, [](AsyncWebServerRequest * request) {
-    request->send(SPIFFS, "/index.html");
+  server.onNotFound([](AsyncWebServerRequest *request) {
+    request->send(404);
   });
 
-  server.on("/index", HTTP_ANY, [](AsyncWebServerRequest * request) {
+  server.on("/", HTTP_ANY, [](AsyncWebServerRequest *request) {
     request->send(SPIFFS, "/index.html");
+    DEBUG_PRINTLN(F("\n### Client connected."));
   });
 
-  server.on("/getcurrentfrequency", HTTP_ANY, [](AsyncWebServerRequest * request) {
+  server.on("/index", HTTP_ANY, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/index.html");
+    DEBUG_PRINTLN(F("\n### Client connected."));
+  });
+
+  server.on("/getcurrentfrequency", HTTP_ANY, [](AsyncWebServerRequest *request) {
     String s = String(FMSTATION / 100);
     s += ".";
     s += String(FMSTATION % 100);
     request->send(200, "plain/text", s);
   });
 
-  server.on("/changefrequency", HTTP_ANY, [](AsyncWebServerRequest * request) {
-    if (request->hasParam("frequency"), true) {
-      AsyncWebParameter* param = request->getParam("frequency", false);
+  server.on("/changefrequency", HTTP_ANY, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("frequency"), true)
+    {
+      AsyncWebParameter *param = request->getParam("frequency", false);
       String param_string = param->value();
       FMSTATION = param_string.toInt();
 
@@ -127,22 +121,21 @@ void initWifi() {
     }
   });
 
-
   server.begin();
   DEBUG_PRINTLN(F("Wifi connection...SUCCESS"));
 }
 
-
-
-
 ///////////////////////////////////////////////////////////////////////////////////////
-void initFmRadio() {
+void initFmRadio()
+{
   DEBUG_PRINT(F("\n\nConnecting to FM radio..."));
-  if (! radio.begin()) {  // begin with address 0x63 (CS high default)
+  if (!radio.begin())
+  { // begin with address 0x63 (CS high default)
     DEBUG_PRINTLN(F("FAILED! Couldn't find radio!"));
     //while (1);
   }
-  else {
+  else
+  {
     DEBUG_PRINTLN("connected.");
   }
   // Uncomment to scan power of entire range from 87.5 to 108.0 MHz
@@ -160,36 +153,34 @@ void initFmRadio() {
   DEBUG_PRINT(F("\nSetting TX power to: "));
   DEBUG_PRINT(MAX_TX_POWER);
   DEBUG_PRINTLN(F(" dBuV"));
-  radio.setTXpower(MAX_TX_POWER);  // dBuV, 88-115 max
+  radio.setTXpower(MAX_TX_POWER); // dBuV, 88-115 max
   printRadioInfo();
 
   // begin the RDS/RDBS transmission
   radio.beginRDS();
   radio.setRDSstation("HackerRadio");
-  radio.setRDSbuffer( "HackerRadio FTW!");
+  radio.setRDSbuffer("HackerRadio FTW!");
   DEBUG_PRINTLN(F("RDS on!"));
   DEBUG_PRINTLN(F("Radio setup complete."));
 
-  radio.setGPIOctrl(_BV(1) | _BV(2));  // set GP1 and GP2 to output
+  radio.setGPIOctrl(_BV(1) | _BV(2)); // set GP1 and GP2 to output
   radio.setGPIO((1 << 2) || (1 << 1));
 }
 
-
-
-
-
 /////////////////////////////////////////////////////
-void initSPIFFS() {
-  if (!SPIFFS.begin()) {
+void initSPIFFS()
+{
+  if (!SPIFFS.begin())
+  {
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
 }
 
-
-
-
-
-void printFrequency(int frequency) {
-  DEBUG_PRINT(frequency / 100); DEBUG_PRINT("."); DEBUG_PRINT(frequency % 100); DEBUG_PRINT(" MHz");
+void printFrequency(int frequency)
+{
+  DEBUG_PRINT(frequency / 100);
+  DEBUG_PRINT(".");
+  DEBUG_PRINT(frequency % 100);
+  DEBUG_PRINT(" MHz");
 }
